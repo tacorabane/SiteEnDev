cf195c5134cdd95120c78b9bd5aca16115941ff8
tacorabane <hustlerox@gmail.com>
2020-07-15 19:45:50 +0200

    Update users.js
    Adding:
    jsonwebtoken
    users model
    customers model

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,15 +1,48 @@
-export.signup = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10).then(hash => {
+const jwt = require('jsonwebtoken');
+const Users = require('../models/users');
+const Customer = require('../models/customer');
+const bcrypt = require('bcrypt');
+
+exports.signup = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)
+  .then(hash => {
     const user = new Users({
       email: req.body.email,
       password: hash
     });
-    user.save()*
+    user.save()
       .then(() => res.status(201).json({message: "User created !"}))
       .catch(error => res.status(400).json({error}));
   }).catch(error => res.status(500).json({error}));
+
+  const customer = new Customer({
+    email: req.body.email
+  });
+
+  customer.save()
+    .then(() => res.status(201).json({message: "Customer created !"}))
+    .catch(error => res.status(400).json({error}));
 };
 
-export.login = (req, res, next) => {
-
+exports.login = (req, res, next) => {
+  Users.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+      }
+      bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+              userId = user._id,
+              'RANDOM_TOKEN_SECRET',
+              {expiresIn: '24h'}
+            )
+          });
+        }).catch(error => res.status(500).json({ error }));
+    }).catch(error => res.status(500).json({ error }));
 };
