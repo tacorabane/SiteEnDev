58e6c957faaa1fb71615b0ef8dbfee81d5525b1a
tacorabane <hustlerox@gmail.com>
2020-07-15 19:44:35 +0200

    Update users.js
    Adding:
    users controller
    body parser

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const userCrtl = require('../controllers/users');
+const bodyParser = require('body-parser');
+const urlEncodeParser = bodyParser.urlencoded({extended: false});
 
-const userCrtl = require('../controllers/user');
+router.post('/signup', urlEncodeParser, userCrtl.signup);
 
-router.post('/signup', userCrtl.signup);
-
-router.post('/login', userCrtl.login);
+router.post('/login', urlEncodeParser, userCrtl.login);
 
 module.exports = router;
