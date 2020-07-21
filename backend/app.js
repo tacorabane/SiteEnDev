27d8d21cba05dc6ee0b40d1f1722e9a5ccb2860f
tacorabane <hustlerox@gmail.com>
2020-07-21 02:38:42 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,18 +11,19 @@ const uri = env.URI + env.USER_CLUSTER + ':' + env.PSW_CLUSTER + env.URL + env.D
 
 //MongoDB Connection
 mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => console.log('Conn DB OK'))
-.catch(() => console.log('Connexion DB failed'));
+    .then(() => console.log('Conn DB OK'))
+    .catch(() => console.log('Connexion DB failed'));
 
 //Erreur CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
 
 app.use('/api/customer', customerRoutes);
+
 app.use('/api/auth', usersRoutes);
 
 module.exports = app;
