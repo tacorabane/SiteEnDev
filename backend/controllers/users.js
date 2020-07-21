82609711f37d890f7ce2638bdc05dfea34a6f820
tacorabane <hustlerox@gmail.com>
2020-07-21 02:38:52 +0200

    Update users.js

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -4,45 +4,53 @@ const Customer = require('../models/customer');
 const bcrypt = require('bcrypt');
 
 exports.signup = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)
-  .then(hash => {
-    const user = new Users({
-      email: req.body.email,
-      password: hash
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new Users({
+            email: req.body.email,
+            password: hash
     });
+        
+    const customer = new Customer({
+        email: req.body.email
+    });
+        
     user.save()
-      .then(() => res.status(201).json({message: "User created !"}))
+      .then(() => customer.save()
+           .then(() => res.status(201).json({message: "User and Customer created !"}))
+           .catch(error => res.status(400).json({error})))
       .catch(error => res.status(400).json({error}));
-  }).catch(error => res.status(500).json({error}));
+    }).catch(error => res.status(500).json({error}));
 
-  const customer = new Customer({
-    email: req.body.email
-  });
-
-  customer.save()
-    .then(() => res.status(201).json({message: "Customer created !"}))
-    .catch(error => res.status(400).json({error}));
 };
 
 exports.login = (req, res, next) => {
-  Users.findOne({ email: req.body.email })
+    Users.findOne({ email: req.body.email })
     .then(user => {
-      if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-      }
-      bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
-          if (!valid) {
-            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+          if (!user) {
+            return res.status(401).json({ error: 'Utilisateur non trouvé !' });
           }
-          res.status(200).json({
-            userId: user._id,
-            token: jwt.sign(
-              userId = user._id,
-              'RANDOM_TOKEN_SECRET',
-              {expiresIn: '24h'}
-            )
-          });
+        bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+            if (!valid) {
+                return res.status(401).json({ error: 'Mot de passe incorrect !' });
+            }
+            
+            var token = jwt.sign({
+                    userId: user._id,
+                }, 'RANDOM_TOKEN_SECRET', {expiresIn: '24h'});
+            
+            res.status(200).json({
+                id: user._id,
+                auth_token: token
+            });
         }).catch(error => res.status(500).json({ error }));
     }).catch(error => res.status(500).json({ error }));
+};
+
+exports.logout = (req, res, next) => {
+    res.status(200).json({
+        auth: false,
+        token: null
+    });
 };
