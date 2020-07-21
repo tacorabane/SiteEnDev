afe06dd1531f6c5854ee62c1fd6bf491850d2279
tacorabane <hustlerox@gmail.com>
2020-07-21 02:37:39 +0200

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,20 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  //console.log(req.headers);
-  try {
-    const token = req.headers.authorization.split(' ')[1];
-    console.log(token);
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    const userId = decodedToken.userId;
-    if(req.body.userId && req.body.userId !== userId) {
-      throw 'Invalid user ID';
-    } else {
-      console.log('Valid user !');
-      next();
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodedToken.userId;
+        
+        if(req.body.userId && req.body.userId !== userId) {
+          throw 'Invalid user ID';
+        } else {
+              console.log('Valid user !');
+              next();
+        }
+    } catch {
+        console.log('Invalid request');
+        res.status(401).json({error: new Error('Invalid request')});
     }
-  } catch {
-    console.log('Invalid request');
-    res.status(401).json({error: new Error('Invalid request')});
-  }
 };
